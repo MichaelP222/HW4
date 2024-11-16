3ba2548bd69424abdbe622d4e2b706ee3b8b62bd
Yinuo Peng <yinuop2@uci.edu>
2024-11-16 01:49:51 +0000

    complete functions of Artist

--- src/artist.cpp
+++ src/artist.cpp
@@ -1,20 +1,37 @@
 // artist.cpp: function definitions file for artist.h
 
 // place header file includes here
+#include "../include/artistList.hpp"
+#include <iostream>
+using namespace std;
 
 // Allocate a new artist record
 Artist::Artist(const std::string & artist_id, const std::string & artist_name, int total_followers, std::string genres[Artist::max_genres], int popularity) {
-  // Insert code here
+  this->artist_id = artist_id;
+  this->artist_name = artist_name;
+  this->total_followers = total_followers;
+  for (std::size_t i = 0; i < max_genres; ++i) {
+        this->genres[i] = genres[i];
+    }
+  this->popularity = popularity;
 }
 
 // Print an artist record
 void Artist::printArtist() const{
-  // Insert code here
+  cout << "Artist ID: " << this->artist_id << endl;
+  cout << "Artist Name: " << this->artist_name << endl;
+  cout << "Total Followers: " << this->total_followers << endl;
+  cout << "Genres: ";
+  for (std::size_t i = 0; i < max_genres; ++i) {
+        cout << this->genres[i];
+    }
+  cout << endl;
+  cout << "Popularity: " << this->popularity << endl;
 }
 
 const std::string& Artist::name() const {
-  // Insert code here
+  return this->artist_name;
 }
 const std::string& Artist::id() const {
-  // Insert code here
+  return this->artist_id;
 }
